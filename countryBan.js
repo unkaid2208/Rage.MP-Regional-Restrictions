2aa691dcd7494e03b14b16dab5f066d1eb9f1700
Vladimir Nikitin <unkaid@gmail.com>
2019-05-22 11:23:13 +0300

    Create countryBan.js

---
+++ countryBan.js
@@ -0,0 +1,25 @@
+var geoip = require('geoip-lite');
+
+var country_ban = [
+    "US",
+    "A1",
+    "A2",
+    "O1",
+    "TR",
+    "NL",
+    "DE"
+];
+
+mp.events.add('playerJoin', player => {
+    var allowed = true;
+    var geo = geoip.lookup(player.ip);
+    var countryID = geo.country;
+
+    if(country_ban.indexOf(countryID) >= 0) {
+        allowed = false; 
+    }
+    if(!allowed) {
+        player.kick('!');
+    }
+    console.log(`[CountryBan Check]: Player: ${player.ip} (${countryID}) can not join due to regional restrictions.`);
+});
